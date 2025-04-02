a3fb2536933e6869d533ac62d44c5531de4437dc
Sam Miller <millersr25@vt.edu>
2025-04-02 17:01:44 -0400

    adding to the mosfetcontrol file and working with tkinter

--- mosfetControl.py
+++ mosfetControl.py
@@ -16,6 +16,13 @@ def StartCharging():
     board.digital[3].write(1)   # turn Digital Pin 3 ON
 def StopCharging():
     board.digital[3].write(0)   # turn Digital Pin 3 OFF
+def ReadLIPOVoltage(): 
+    rawValue = board.analog[0].read() # directly read from the A0 pin
+    if rawValue is not None:
+        lipoVoltage = rawValue * 5.0 
+        return lipoVoltage
+    else:
+        return None
 #===============================================
 # Arduino board connected to serial port COM3
 board = Arduino("COM3")
@@ -23,5 +30,18 @@ board = Arduino("COM3")
 # Root widget to create window
 win = tk.Tk()
 # initialize window with title & minimum size
-win.title("L E D")
+win.title("Solar Harvester Program")
 win.minsize(200,60)
+
+# Label widget
+label = tk.Label(win, text="click to turn ON/OFF")
+label.grid(column=1, row=1)
+
+# Button widget
+ONbtn = tk.Button(win, bd=4, text="LED ON", command=SendStartUpVoltage)
+ONbtn.grid(column=1, row=2)
+OFFbtn = tk.Button(win, bd=4, text="LED OFF", command=StopCharging)
+OFFbtn.grid(column=2, row=2)
+
+# start & open window continously
+win.mainloop()
---
+++ test_tkinter.py
@@ -0,0 +1,5 @@
+from tkinter import * 
+tk = Tk()
+w = Label(tk, text="Sam's Test window")
+w.pack()
+tk.mainloop()
